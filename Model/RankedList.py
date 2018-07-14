ad42b558251664ac7e59293d4f5d0e962697fe23
JoshBasserabie <josh.basserabie@gmail.com>
2018-07-15 09:56:20 +1000

    updateScore function written

--- Model/RankedList.py
+++ Model/RankedList.py
@@ -1,3 +1,5 @@
+from __future__ import division
+
 class RankedList:
 
     def __init__(self):
@@ -9,6 +11,8 @@ class RankedList:
         votingList = []
         # List of item references
         items = []
+        # number of items
+        itemNum = 0
 
     def addItem(self, name):
         if name is None:
@@ -20,6 +24,7 @@ class RankedList:
         self.votingList.append({})
         self.items.append(newItem)
         self.scoreList.append(0)
+        self.itemNum += 1
         return True
 
     def handleVote(self, winnerID, loserID):
@@ -27,13 +32,24 @@ class RankedList:
         if voteDictionary[loserID] is None:
             voteDictionary[loserID] = 0
         voteDictionary[loserID] += 1
-        updateScore(winnerID)
-        updateScore(loserID)
-        updateSortedList(winnerID)
-        updateSortedList(loserID)
+        self.updateScore(winnerID)
+        self.updateScore(loserID)
+        self.updateSortedList(winnerID)
+        self.updateSortedList(loserID)
 
     def updateScore(self, itemID):
         #update the score for itemID
+        score = 0.0
+        count = 0
+        votes = self.votingList[itemID]
+        for i in range(len(self.items)):
+            currVotes = self.votingList[i]
+            if i != itemID:
+                if votes[i] != 0 or currVotes[itemID] != 0:
+                    count += 1
+                score += votes[i] / (votes[i] + currVotes[itemID])
+        score /= count
+        self.scoreList[itemID] = score
 
     def updateSortedList(self, itemID):
         #fix the sorted list by moving itemID until it is in a valid position
