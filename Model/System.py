29009f58d08ffbe4e7671a62107a8a3175816b17
JoshBasserabie <josh.basserabie@gmail.com>
2018-07-15 13:29:17 +1000

    createList now returns list

--- Model/System.py
+++ Model/System.py
@@ -8,8 +8,8 @@ class System:
         newList = RankedList(listName, listItemNames)
         if newList is not None:
             self.rankedLists.append(newList)
-            return True
-        return False
+            return newList
+        return None
 
     def deleteList(self, listID):
         if listID > len(self.rankedLists) or listID < 0:
@@ -17,8 +17,3 @@ class System:
         self.rankedLists[listID], self.rankedLists[-1] = self.rankedLists[-1], self.rankedLists[listID]
         self.rankedLists.pop()
         return True
-
-    def getList(self, listID):
-        if listID > len(self.rankedLists) or listID < 0:
-            return None
-        return self.rankedLists[listID]
